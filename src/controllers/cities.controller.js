b749a0bddc3734e3e0ec4e854558dfd5e05e3154
yokuny <felipe.vni@hotmail.com>
2023-05-26 16:11:43 -0300

    first query to get cities

--- src/controllers/cities.controller.js
+++ src/controllers/cities.controller.js
@@ -1,4 +1,13 @@
+import db from "../database/db.database.js";
 const getCities = async (req, res) => {
-  return res.send("getCities");
+  const query = "SELECT nome, id FROM cidades";
+  try {
+    const { rows: cities } = await db.query(query);
+
+    return res.status(200).json(cities);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 };
 export default { getCities };
